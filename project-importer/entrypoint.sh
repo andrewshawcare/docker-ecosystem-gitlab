0bb835954ba39905aa11dd4340726d9b6b65e605
Andrew Shaw Care <andrew@andrews-mbp.lan>
2017-10-26 21:04:31 -0400

    Add downstream project tokens to create multi-project pipelines

--- project-importer/entrypoint.sh
+++ project-importer/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wait_for_server() {
-  until curl --fail --location http://gitlab-ce/api/v4/projects; do
+  sleep 180
+  until [ "$(curl --silent --output /dev/null --write-out '%{http_code}' http://gitlab-ce/api/v4/session)" -eq 404 ]; do
     sleep 30
   done
 }
@@ -23,18 +24,6 @@ create_project() {
     --header 'Content-Type: application/json' \
     --header "Private-Token: ${private_token}" \
     --data "{\"name\": \"${name}\", \"import_url\": \"${import_url}\"}" \
-    http://gitlab-ce/api/v4/projects
-}
-
-search_for_projects_by_name() {
-  private_token=$1
-  search=$2
-
-  curl \
-    --request GET \
-    --get \
-    --header "Private-Token: ${private_token}" \
-    --data "search=${search}" \
     http://gitlab-ce/api/v4/projects
 }
 
@@ -65,8 +54,35 @@ trigger_project_pipeline() {
     "http://gitlab-ce/api/v4/projects/${project_id}/trigger/pipeline"
 }
 
+downstream_project_names() {
+  project_name=$1
+  case $project_name in
+    'docker-ecosystem-migration' ) echo 'docker-ecosystem-java-service docker-ecosystem-node-service' ;;
+    'docker-ecosystem-java-service' ) echo 'docker-ecosystem-client' ;;
+    'docker-ecosystem-node-service' ) echo 'docker-ecosystem-client' ;;
+    'docker-ecosystem-client' ) echo '' ;;
+  esac
+}
+
+create_project_level_variable() {
+  private_token=$1
+  project_id=$2
+  key=$3
+  value=$4
+
+  curl \
+    --request POST \
+    --header 'Content-Type: application/json' \
+    --header "Private-Token: ${private_token}" \
+    --data "{\"key\": \"${key}\", \"value\": \"${value}\"}" \
+    "http://gitlab-ce/api/v4/projects/${project_id}/variables"
+}
+
 wait_for_server
 
+private_token="$(create_session | jq --raw-output '.private_token')"
+
+namespace='root'
 project_names=$(cat <<'HEREDOC'
 docker-ecosystem-migration
 docker-ecosystem-java-service
@@ -74,16 +90,28 @@ docker-ecosystem-node-service
 docker-ecosystem-client
 HEREDOC
 )
-private_token="$(create_session | jq --raw-output '.private_token')"
 git_origin='https://github.com/andrewshawcare'
 
+# Create projects
 for project_name in ${project_names}; do
   create_project "${private_token}" "${project_name}" "${git_origin}/${project_name}"
 done
 
-initial_project_id="$(search_for_projects_by_name "${private_token}" 'docker-ecosystem-migration' | jq --raw-output '.[0].id')"
+# Create downstream project triggers and add them as project-level variables
+for project_name in ${project_names}; do
+  for downstream_project_name in $(downstream_project_names $project_name); do
+    project_id="${namespace}%2f${project_name}"
+    downstream_project_id="${namespace}%2f${downstream_project_name}"
+    trigger_token="$(create_project_pipeline_trigger ${private_token} ${downstream_project_id} ${project_name} | jq --raw-output '.token')"
+    key="$(echo "${downstream_project_name}_TRIGGER_TOKEN" | tr '-' '_' | tr '[:lower:]' '[:upper:]')"
+    create_project_level_variable ${private_token} ${project_id} ${key} ${trigger_token}
+  done
+done
 
-trigger="$(create_project_pipeline_trigger ${private_token} ${initial_project_id} 'Initial project trigger')"
-trigger_token="$(echo ${trigger} | jq --raw-output '.token')"
+# Trigger initial pipeline
+initial_project_name='docker-ecosystem-migration'
+initial_project_id="${namespace}%2f${initial_project_name}"
+initial_trigger="$(create_project_pipeline_trigger ${private_token} ${initial_project_id} 'Initial project trigger')"
+initial_trigger_token="$(echo ${initial_trigger} | jq --raw-output '.token')"
 
-trigger_project_pipeline ${private_token} ${initial_project_id} ${trigger_token} 'master'
+trigger_project_pipeline ${private_token} ${initial_project_id} ${initial_trigger_token} 'master'
